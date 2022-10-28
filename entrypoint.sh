1a90f6db59429be922251679250b4c8628e2431c
onkow <116881995+onkow@users.noreply.github.com>
2022-10-28 05:12:11 -0700

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+# args
+AUUID="7bf357e0-5f10-4afb-a2e5-bc0cee0fc6de"
+CADDYIndexPage="https://github.com/AYJCSGM/mikutap/archive/master.zip"
+ParameterSSENCYPT="chacha20-ietf-poly1305"
+
+# configs
+mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
+wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
+cat /conf/Caddyfile | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
+mkdir -p /etc/xray/ /usr/share/xray
+wget -O /usr/share/xray/geosite.dat https://github.com/v2fly/domain-list-community/releases/latest/download/dlc.dat 
+wget -O /usr/share/xray/geoip.dat https://github.com/v2fly/geoip/releases/latest/download/geoip.dat
+cat /conf/xray.json | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/etc/xray/config.json
+
 # start
 tor &
 xray -config /etc/xray/config.json &
